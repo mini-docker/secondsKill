e9fa8c7b0260776aa10c66ec3079e79776cbcba2
js103 <643929860@qq.com>
2019-10-12 13:52:02 +0700

    rewrite product_repository.go 1

---
+++ product-dtest/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"github.com/kataras/iris"
+
+)
+
+func main() {
+	//1.创建iris 实例
+	app := iris.New()
+	//2.设置错误模式，在mvc模式下提示错误
+	app.Logger().SetLevel("debug")
+	//3.注册模板
+	tmplate := iris.HTML("./backend/web/views", ".html").Layout("shared/layout.html").Reload(true)
+	app.RegisterView(tmplate)
+	//4.设置模板目标
+	app.StaticWeb("/assets", "./backend/web/assets")
+	//出现异常跳转到指定页面
+	app.OnAnyErrorCode(func(ctx iris.Context) {
+		ctx.ViewData("message", ctx.Values().GetStringDefault("message", "访问的页面出错！"))
+		ctx.ViewLayout("")
+		ctx.View("shared/error.html")
+	})
+	//连接数据库
+	//db, err := common.NewMysqlConn()
+	//if err != nil {
+	//	log.Error(err)
+	//}
+	//ctx, cancel := context.WithCancel(context.Background())
+	//defer cancel()
+	//
+	////5.注册控制器
+	//productRepository := repositories.NewProductManager("product", db)
+	//productSerivce := services.NewProductService(productRepository)
+	//productParty := app.Party("/product")
+	//product := mvc.New(productParty)
+	//product.Register(ctx, productSerivce)
+	//product.Handle(new(controllers.ProductController))
+
+	//6.启动服务
+	app.Run(
+		iris.Addr("localhost:8082"),
+		//iris.Without,
+		iris.WithoutServerError(iris.ErrServerClosed),
+		iris.WithOptimizations,
+	)
+
+}
